9613e7e628bf8b736d6c88d27ef9ac1297a8e9a1
arzzen <lukas.mestan@gmail.com>
2015-05-26 22:44:21 +0200

    annotation fix

--- TimeLockCrypt.php
+++ TimeLockCrypt.php
@@ -4,44 +4,103 @@ require "fernet-php/src/Fernet.php";
 use Fernet\Fernet;
 
 /**
- * Time-Lock encryption class
+ * Time Lock Encryption Class
  *
  * @author Lukas Mestan
  * @copyright GPL v2
  * @see http://www.gwern.net/Self-decrypting files
  * @see http://people.csail.mit.edu/rivest/lcs35-puzzle-description.txt
+ * @see https://github.com/kelvinmo/fernet-php 
  * @version 1.0.0
  */
 class TimeLockCrypt
 {
+	/**
+	 * Count of iterations
+	 *
+	 * @access private
+	 * @var integer
+	 */
 	private $iterations = 0;
 
+	/**
+	 * Keyseed
+	 *
+	 * @access private
+	 * @var string
+	 */
 	private $keyseed = ''; 
+	/**
+	 * Signing key
+	 *
+	 * @access private
+	 * @var string
+	 */
 	private $signing_key = '';
 
 
+	/**
+	 * Class contructor
+	 *
+	 * @access public
+	 * @param string $keyseed 
+	 */
 	public function __construct($keyseed)
 	{
 		$this->keyseed = $keyseed;
 		$this->signing_key = substr($keyseed, 0, 16);
 	}
 
-
+	/**
+	 * Encrypt message
+	 *
+	 * @access public
+	 * @param string $delta
+	 * @param string $message 
+	 * @return string
+	 */
 	public function encrypt($delta, $message)
 	{
 		$key = $this->generateByTime($this->keyseed, $delta);
+		$fernet = new Fernet($key);
 
-		return (new Fernet($key))->encode($message);
+		return $fernet->encode($message);
 	}
 
+	/**
+	 * Decrypt message
+	 *
+	 * @access public
+	 * @param string $encrypted 
+	 * @return string
+	 */
 	public function decrypt($encrypted)
 	{
 		$key = $this->generateByIterations($this->keyseed, $this->iterations);
+		$fernet = new Fernet($key);
 
-		return (new Fernet($key))->decode($encrypted);
+		return $fernet->decode($encrypted);
 	}
 
+	/**
+	 * Iterations number
+	 *
+	 * @access public
+	 * @return integer
+	 */
+    public function getIterations()
+    {
+        return $this->iterations;
+    }
 
+	/**
+	 * Generate key by iterations
+	 *
+	 * @access private
+	 * @param string $seed
+	 * @param integer $iters
+	 * @return string
+	 */
 	private function generateByIterations($seed, $iters)
 	{
 		$hash = hash_hmac("sha256", $seed, $this->signing_key, TRUE);
@@ -54,6 +113,14 @@ class TimeLockCrypt
 		return Fernet::base64url_encode($hash);
 	}
 
+	/**
+	 * Generate key by time
+	 *
+	 * @access private
+	 * @param string $seed
+	 * @param integer|string $delta strtotime 
+	 * @return string
+	 */
 	private function generateByTime($seed, $delta)
 	{
 		$end = strtotime($delta);
@@ -70,10 +137,5 @@ class TimeLockCrypt
 
 		return Fernet::base64url_encode($hash);
 	}
-
-    public function getIterations()
-    {
-        return $this->iterations;
-    }
 
 }
